66aec821481e461800b15f04d3692847fa9bd1e0
Renuka2608 <renukasonawane03@gmail.com>
2026-03-03 10:21:20 +0530

    Add RunAction header for dose accumulation and run management

---
+++ include/RunAction.hh
@@ -0,0 +1,25 @@
+#ifndef RunAction_h
+#define RunAction_h 1
+
+#include "G4VUserRunAction.hh"
+#include "G4Accumulable.hh"
+#include "globals.hh"
+
+class G4Run;
+
+class RunAction : public G4VUserRunAction
+{
+public:
+	RunAction();
+	~RunAction() override = default;
+
+	void BeginOfRunAction(const G4Run*) override;
+	void EndOfRunAction(const G4Run*) override;
+
+	void AddEdep(G4double edep);
+	
+private:
+	G4Accumulable<G4double> fEdep = 0.;     //fEdep = total deposited energy      
+	G4Accumulable<G4double> fEdep2 = 0.;   //fEdep2 = used to calculate RMS / statistical error
+};
+#endif 
