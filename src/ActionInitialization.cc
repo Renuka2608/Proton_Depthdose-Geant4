092c4ee8e00c3ec5ae5d6d1316efff577209bef0
Renuka2608 <renukasonawane03@gmail.com>
2026-03-05 11:00:27 +0530

    Implement ActionInitialization to register primary, run, event, and stepping actions

---
+++ src/ActionInitialization.cc
@@ -0,0 +1,25 @@
+#include "ActionInitialization.hh"
+
+#include "EventAction.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "SteppingAction.hh"
+
+void ActionInitialization::BuildForMaster() const
+{
+	auto runAction = new RunAction;
+	SetUserAction(runAction);
+}
+
+void ActionInitialization::Build() const
+{
+	SetUserAction(new PrimaryGeneratorAction());
+
+	auto runAction = new RunAction;
+	SetUserAction(runAction);
+
+	auto eventAction = new EventAction(runAction);
+	SetUserAction(eventAction);
+
+	SetUserAction(new SteppingAction(eventAction));
+}
