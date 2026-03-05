29061d3fd3a2336cf0ec516ade8904e1078bd5a8
Renuka2608 <renukasonawane03@gmail.com>
2026-03-05 22:29:51 +0530

    Update

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -1,76 +1,81 @@
 #include "DetectorConstruction.hh"
 
-#include "G4NistMnager.hh"
+#include "G4NistManager.hh"
 #include "G4Box.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
 
 // Constructor & Destructor
-DetectorConstruction::DetectorConstruction(): G4DetectorConstruction();
- fPhantomLogical(nullptr)
+DetectorConstruction::DetectorConstruction()
+	:G4VDetectorConstruction(),
+	fScoringVolume(nullptr)
 { }
 
-DetectorConstruction::~DetectorConstruction() {}
+DetectorConstruction::~DetectorConstruction() = default;
 
-G4PhysicalVolume* DetectorConstruction::Construct()
+G4VPhysicalVolume* DetectorConstruction::Construct()
 {
 
 //NIST material manager
-	G4NistManager* nist = G4NistManager::Instance()
+	G4NistManager* nist = G4NistManager::Instance();
 
-		// Materials
-	G4Material * worldMaterial = nist->FindOrBuildMaterial("G4_AIR");
+// Materials
+	G4Material* worldMaterial = nist->FindOrBuildMaterial("G4_AIR");
 	G4Material* phantomMaterial = nist->FindOrBuildMaterial("G4_WATER");
 
 
 // World Volume
-	G4double worldSizeXY = 50*cm;
-	G4double worldSizeZ = 50*cm;
+	G4double worldSizeX = 1*m;
+	G4double worldSizeY = 1*m;
+	G4double worldSizeZ = 1*m;
 
-	G4Box* solidWorld =
-		new G4Box("World",
-			worldSizeXY / 2,
-			worldSizeXY / 2,
-			worldSizeZ / 2);
+	// Define Shape 
+	G4Box* solidWorld = new G4Box("World",
+							worldSizeX / 2,
+							worldSizeY / 2,
+							worldSizeZ / 2);
 
-	G4LogicalVolume* logicWorld =
-		new G4LogicalVolume(solidWorld,
-							worldMaterial,
-							"World");
+	// Define Logical volume 
+	G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld,
+													worldMaterial,
+													"World");
+		
 
-	G4VPhysicalVolume* physworld =
-		new G4PVPlacement(0,
-			G4Vector(),
-			logicWorld,
-			"World",
-			0,
-			false,
-			0,
-			true);
+	// Place it
+	G4VPhysicalVolume* physWorld = new G4PVPlacement(0,                      // no rotation
+													G4ThreeVector(),         // at (0,0,0)
+													logicWorld,
+													"World",
+													0,                      // it's mother volume
+													false,                  // no boolean operation- parameter : G4bool pMany
+													0,						// copy number
+													true);					// overlaps checking
+		
 
 //Water Phantom
-	G4double worldSizeXY = 10 * cm;
-	G4double worldSizeZ = 30 * cm;
+	G4double phantomSizeX = 20 * cm;
+	G4double phantomSizeY = 20 * cm;
+	G4double phantomSizeZ = 30 * cm;
 
-	G4Box* solidPhantom =
-		new G4Box("Phantom",
-			worldSizeXY / 2,
-			worldSizeXY / 2,
-			worldSizeZ / 2);
+	G4Box* solidPhantom = new G4Box("Phantom",
+									worldSizeX / 2,
+									worldSizeY / 2,
+									worldSizeZ / 2);
+		
 
-	fPhantomLogical =
-		new G4LogicalVolume(solidPhantom,
-			phantomMaterial,
-			"Phantom");
+	fScoringVolume = new G4LogicalVolume(solidPhantom,
+										phantomMaterial,
+										"Phantom");
+		
 
 		new G4PVPlacement(0,
-						G4Vector(0,0,0),
-						fPhantomLogical,
+						G4ThreeVector(0,0,15*cm),
+						fScoringVolume,
 						"Phantom",
-						logicWorld,
-						false,
-						0,
+						logicWorld,               // it's mother volume. Envelope is inside World.
+						false,			   		  // means This is not part of a multiple placement scheme.
+						0,					      //G4int copyNo - Copy number is used when you place multiple identical volumes.
 						true);
 
 		return phyWorld;
