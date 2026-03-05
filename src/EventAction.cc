7b7258d775fd9b727c137067e5d5fc662f88c97b
Renuka2608 <renukasonawane03@gmail.com>
2026-03-05 18:53:26 +0530

    Add EventAction implementation to accumulate event energy deposition and pass to RunAction

---
+++ src/EventAction.cc
@@ -0,0 +1,16 @@
+#include "EventAction.hh"
+#include "RunAction.hh"
+
+//Constructor definition
+
+EventAction::EventAction(RunAction* runAction) : fRunAction(runAction) {}
+
+void EventAction::BeginOfEventAction(const G4Event*)
+{
+	fEdep = 0.;
+}
+
+void EventAction::EndOfEventAction(const G4Event*)
+{
+	fRunAction->AddEdep(fEdep);
+}
