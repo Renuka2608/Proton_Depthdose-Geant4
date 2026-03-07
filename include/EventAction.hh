dd1e42236c3c04589b740277ce0721c0800007ce
Renuka2608 <renukasonawane03@gmail.com>
2026-03-08 01:15:30 +0530

    Typo

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -5,20 +5,20 @@
 #include "globals.hh"
 
 class G4Event;
-class RunACtion;
+class RunAction;
 
 class EventAction : public G4UserEventAction
 {
-public:
-	EventAction(RunAction* runAction);           //EventAction receives a pointer to RunAction.
-	~EventAction() override = default;
+  public:
+	EventAction(RunAction* runAction);
+    ~EventAction() override = default;
 
 	void BeginOfEventAction(const G4Event* event) override;
 	void EndOfEventAction(const G4Event* event) override;
 
 	void  AddEdep(G4double edep) { fEdep += edep; }
 
-private:
+  private:
 	RunAction* fRunAction = nullptr;
 	G4double fEdep = 0.;
 };
