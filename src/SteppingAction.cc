d6d82bec280c652d65cecd5e813485ca55509758
Renuka2608 <renukasonawane03@gmail.com>
2026-03-08 01:23:09 +0530

    Included fstream

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -6,7 +6,9 @@
 #include "G4LogicalVolume.hh"
 #include "G4RunManager.hh"
 #include "G4Step.hh"
-#include "G4AnalysisManager.hh"
+//#include "G4AnalysisManager.hh"
+#include "G4SystemOfUnits.hh"
+#include <fstream>
 
 
 SteppingAction::SteppingAction(EventAction* eventAction): fEventAction(eventAction) {}         //SteppingAction can send energy deposition data to EventAction
@@ -20,7 +22,7 @@ void SteppingAction::UserSteppingAction(const G4Step* step)
 	}
 
 	// get volume of the current step
-	G4LogiclaVolume* colume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();        //checks Where is the particle currently stepping
+	G4LogicalVolume* volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();        //checks Where is the particle currently stepping
 
 	//check if we are in scoring volume
 	if (volume != fScoringVolume) return;
@@ -28,10 +30,19 @@ void SteppingAction::UserSteppingAction(const G4Step* step)
 	//record Energy Deposition
 	G4double edepStep = step->GetTotalEnergyDeposit();
 
-	G4double z = step->GetPreStepPoint()->GetPosition().z();     //3Send Data to Histogram
+	G4double z = step->GetPreStepPoint()->GetPosition().z(); 
+	
 
-	auto analysisManager = G4AnalysisManager::Instance();
+		std::ofstream file;
+        file.open("braggPeak.csv", std::ios::app);
+		file << z / mm << "," << edepStep / MeV << std::endl;
+		file.close();
+	                               //3Send Data to Histogram
+
+	/*auto analysisManager = G4AnalysisManager::Instance();
 	analysisManager->FillH1(0, z / mm, edepStep / MeV);
+	*/
+		G4cout << "Step detected at z=" << z / mm << G4endl;
 
 	fEventAction->AddEdep(edepStep);                        //send energy to eventAction
 
