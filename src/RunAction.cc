4cad0cb71404cb1fbf575222bb1919ba4226f115
Renuka2608 <renukasonawane03@gmail.com>
2026-03-08 01:22:00 +0530

    Included fstream for braggpeak.csv file

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -12,26 +12,42 @@
 #include "G4SystemOfUnits.hh"
 #include "G4UnitsTable.hh"
 #include <cmath>
-#include "G4AnalysisManager.hh"
+//#include "G4AnalysisManager.hh"
+#include "G4Accumulable.hh"
+#include <fstream>
 
 
 RunAction::RunAction()
 {
+		const G4double milligray = 1.e-3 * gray;
+		const G4double microgray = 1.e-6 * gray;
+		const G4double nanogray = 1.e-9 * gray;
+		const G4double picogray = 1.e-12 * gray;
+
+		new G4UnitDefinition("milligray", "milliGy", "Dose", milligray);
+		new G4UnitDefinition("microgray", "microGy", "Dose", microgray);
+		new G4UnitDefinition("nanogray", "nanoGy", "Dose", nanogray);
+		new G4UnitDefinition("picogray", "picoGy", "Dose", picogray);
+	
+
 	//Register accumulables (total energy deposit and variance)
 	auto accumulableManager = G4AccumulableManager::Instance();
-	accumulableManager->Regiter(fEdep);
-	accumulableManager->Regiter(fEdep2);
+	accumulableManager->Register(fEdep);
+	accumulableManager->Register(fEdep2);
 
-	auto analysisManager = G4AnalysisManager::Instance();
+	/*auto analysisManager = G4AnalysisManager::Instance();
+	analysisManager->SetVerboseLevel(1);
+	analysisManager->SetDefaultFileType("csv");
+	*/
 
-	analysisManager->CreateH1(
+	/*analysisManager->CreateH1(
 		"depthDose",
 		"Depth Dose Distribution",
 		300,
 		0.,                                 //300 bins and (0–300 mm) phantom
-		300
-	);
-		
+		300*mm
+	);*/
+	
 }
 void RunAction::BeginOfRunAction(const G4Run*)
 {
@@ -42,7 +58,23 @@ void RunAction::BeginOfRunAction(const G4Run*)
 	auto accumulableManager = G4AccumulableManager::Instance();
 	accumulableManager->Reset();                                  //Resets total energy before starting the run
 
-	analysisManager->OpenFile("braggPeak");                       //open root file
+	//auto analysisManager = G4AnalysisManager::Instance();
+	//analysisManager->OpenFile("braggPeak");                     //open root file
+
+	G4cout << "BeginOfRunAction executed" << G4endl;
+
+	outFile.open("braggPeak.csv");
+	if (!outFile.is_open())
+	{
+		G4cout << "ERROR: CSV file could not open!" << G4endl;
+	}
+	else
+	{
+		G4cout << "CSV file opened successfully!" << G4endl;
+		outFile << "Depth(mm),EnergyDeposit(MeV)" << std::endl;
+	}
+	
+
 }
 
 void RunAction::EndOfRunAction(const G4Run* run)
@@ -84,13 +116,17 @@ void RunAction::EndOfRunAction(const G4Run* run)
 		<< G4endl;
 	G4cout << "=============================================" << G4endl;
 
+	/*auto analysisManager = G4AnalysisManager::Instance();
 	analysisManager->Write();
 	analysisManager->CloseFile();              //save root file
+	*/
+	outFile.close();
 
+	G4cout << "CSV file initialized: braggPeak.csv" << G4endl;
 }
 
 void RunAction::AddEdep(G4double edep)
 {
 	fEdep += edep;       //Adds event energy to total run energy
-	fEdep2 += edep* edep;
+	fEdep2 += edep * edep;
 }
