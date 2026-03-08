548abe3c32526a7a571ec30fe07a39a27f7b96c1
Renuka2608 <renukasonawane03@gmail.com>
2026-03-08 11:06:24 +0530

    added RunAction* member

--- include/SteppingAction.hh
+++ include/SteppingAction.hh
@@ -2,21 +2,25 @@
 #define SteppingAction_h 1
 
 #include "G4UserSteppingAction.hh"
+#include "globals.hh"
+
 
 class G4LogicalVolume;       //Forward Declaration
 class G4Step;
 class EventAction;
+class RunAction;
 
 class SteppingAction : public G4UserSteppingAction
 {
 public:
-	SteppingAction(EventAction* eventAction);  //SteppingAction receives a pointer to EventAction. Because SteppingAction must send energy deposition to EventAction.
+	SteppingAction(EventAction* eventAction , RunAction* runAction);  //SteppingAction receives a pointer to EventAction. Because SteppingAction must send energy deposition to EventAction.
 	~SteppingAction() override = default;
 
 	void UserSteppingAction(const G4Step*) override;
 
 private:
 	EventAction* fEventAction = nullptr;
+	RunAction* fRunAction = nullptr;
 	G4LogicalVolume* fScoringVolume = nullptr;
 };
 #endif
