9b1bc15ca3931b8e79a64244cc380ea5e8ffad4f
Renuka2608 <renukasonawane03@gmail.com>
2026-03-08 11:09:57 +0530

    Add WriteCSVRow() declaration

--- include/RunAction.hh
+++ include/RunAction.hh
@@ -10,8 +10,6 @@ class G4Run;
 
 class RunAction : public G4UserRunAction
 {
-	std::ofstream outFile;
-
 public:
 	RunAction();
 	~RunAction() override = default;
@@ -21,8 +19,12 @@ public:
 
 	void AddEdep(G4double edep);
 	
+	// SteppingAction calls this to write a row
+	void WriteCSVRow(G4double z_mm, G4double edep_MeV);
+
 private:
 	G4Accumulable<G4double> fEdep = 0.;     //fEdep = total deposited energy      
 	G4Accumulable<G4double> fEdep2 = 0.;   //fEdep2 = used to calculate RMS / statistical error
+	std::ofstream outFile;
 };
 #endif 
